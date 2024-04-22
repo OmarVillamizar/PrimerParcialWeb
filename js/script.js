741ac02dd3877ea7179629aaa7228a1a1410224f
Omar Villamizar <omarjesusvi@ufps.edu.co>
2024-04-22 11:03:20 -0500

    funciono el login

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.querySelector('.login');
+//vH6uSk
+document.querySelector('.login').addEventListener('submit', function (event) {
+    event.preventDefault();
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
+    const codigo = document.getElementById('codigo').value;
+    const clave = document.getElementById('clave').value;
 
-        const codigo = document.getElementById('codigo').value;
-        const password = document.getElementById('password').value;
-
-        const data = { codigo, password };
-
-        fetch('https://24a0dac0-2579-4138-985c-bec2df4bdfcc-00-3unzo70c406dl.riker.replit.dev/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-        .then(response => response.json())
-        .then(user => {
-            // Aquí puedes guardar el usuario en localStorage
-            localStorage.setItem('user', JSON.stringify(user));
-            // Luego redireccionar a otra página o realizar alguna acción
-            window.location.href = 'otra_pagina.html';
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            // Aquí puedes mostrar un mensaje de error en la interfaz
-        });
+    fetch('https://24a0dac0-2579-4138-985c-bec2df4bdfcc-00-3unzo70c406dl.riker.replit.dev/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ codigo, clave })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.login) {
+            localStorage.setItem('user', JSON.stringify(data));
+            window.location.href = 'notas.html';
+            alert('Login exitoso');
+        } else {
+            alert(data.mensaje);
+            document.getElementById('codigo').value = '';
+            document.getElementById('clave').value = '';
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert(' las credenciales no son válidas');
     });
 });
