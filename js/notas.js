4da05557f0b837a66710a93904f59a50d57e8ac3
Omar Villamizar <omarjesusvi@ufps.edu.co>
2024-04-22 11:48:43 -0500

    terminando estilos

--- js/notas.js
+++ js/notas.js
@@ -8,12 +8,19 @@ document.addEventListener('DOMContentLoaded', () => {
             .then(data => {
                 const notas = data.notas;
                 const tbody = document.querySelector('#notas-table tbody');
+                let totalCreditos = 0;
+                let sumatoriaDefCreditos = 0;
                 notas.forEach(nota => {
                     const def = ((parseFloat(nota.n1) + parseFloat(nota.n2) + parseFloat(nota.n3)) / 3) * 0.7 + (parseFloat(nota.ex) * 0.3);
                     const row = document.createElement('tr');
                     row.innerHTML = `<td>${nota.asignatura}</td><td>${nota.creditos}</td><td>${nota.n1}</td><td>${nota.n2}</td><td>${nota.n3}</td><td>${nota.ex}</td><td>${def.toFixed(2)}</td>`;
                     tbody.appendChild(row);
+                    totalCreditos += parseFloat(nota.creditos);
+                    sumatoriaDefCreditos += parseFloat(nota.creditos) * def;
                 });
+                const promedio = sumatoriaDefCreditos / totalCreditos;
+                const promedioDiv = document.getElementById('promedio');
+                promedioDiv.textContent = `Promedio: ${promedio.toFixed(2)}`;
             })
             .catch(error => console.error('Error al obtener las notas:', error));
     } else {
